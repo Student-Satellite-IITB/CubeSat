5f311501e1a6f6a8a78407fdbc5af20f176d56d1
Ronit <chitreronit@gmail.com>
2022-07-13 00:18:04 +0530

    Update lqr_controller, lqr_dynamics, main, lqr_constaints
    Improved code for importing files
    Update lqr_constants: Shifted some parameters from lqr_controller to lqr_constants

--- Controller/LQR/lqr_controller.py
+++ Controller/LQR/lqr_controller.py
@@ -1,23 +1,25 @@
 from scipy.linalg import sqrtm
+import numpy as np
+import lqr_constants
 
-from lqr_constants import *
 
-m_Q1 = np.diag((1, 1, 1))
-m_Q2 = np.diag((1, 1, 1))
-m_Q = np.diag(np.concatenate((m_Q1, m_Q2)))
-scal_R = 0.001
-m_R = scal_R * m_Q2
-m_F11 = np.dot(m_I, sqrtm(m_R)).dot(sqrtm(m_Q2)) + np.dot(sqrtm(m_Q2), sqrtm(m_R)).dot(m_I)
-m_F11 = m_Q1 + 0.5 * m_F11
-m_F11 = np.dot(m_I, sqrtm(m_R), sqrtm(m_F11))
-m_F12 = np.dot(m_I, sqrtm(m_R), sqrtm(m_Q2))
-m_F22 = m_Q1 + np.dot(m_I, sqrtm(m_R), sqrtm(m_Q2))
-m_F22 = 2 * np.dot(sqrtm(m_Q2), sqrtm(m_F22))
-F11_F12 = np.concatenate((m_F11, m_F12), axis=1)
-F12_F22 = np.concatenate((m_F12.T, m_F22), axis=1)
-m_F = np.concatenate((F11_F12, F12_F22))
+def initialize_gain():
+    m_R = lqr_constants.scal_R * lqr_constants.m_Q2
+    m_F11 = np.dot(lqr_constants.m_I, sqrtm(m_R)).dot(sqrtm(lqr_constants.m_Q2))
+    m_F11 += np.dot(sqrtm(lqr_constants.m_Q2), sqrtm(m_R)).dot(lqr_constants)
+    m_F11 = lqr_constants.m_Q1 + 0.5 * m_F11
+    m_F11 = np.dot(lqr_constants, sqrtm(m_R), sqrtm(m_F11))
+    m_F12 = np.dot(lqr_constants, sqrtm(m_R), sqrtm(lqr_constants.m_Q2))
+    m_F22 = lqr_constants.m_Q1 + np.dot(lqr_constants, sqrtm(m_R), sqrtm(lqr_constants.m_Q2))
+    m_F22 = 2 * np.dot(sqrtm(lqr_constants.m_Q2), sqrtm(m_F22))
+    F11_F12 = np.concatenate((m_F11, m_F12), axis=1)
+    F12_F22 = np.concatenate((m_F12.T, m_F22), axis=1)
+    m_F = np.concatenate((F11_F12, F12_F22))
+    return -1 * np.dot(np.linalg.inv(m_R), lqr_constants.m_B.T).dot(m_F)
+
+
+gain = initialize_gain()
 
 
 def control_law(x):
-    gain = np.dot(np.linalg.inv(m_R), m_B.T).dot(m_F)
-    return -1 * np.dot(gain, x)
+    return np.dot(gain, x)
