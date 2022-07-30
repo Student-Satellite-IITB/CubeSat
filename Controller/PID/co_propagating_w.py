875441e5ebe8a2684ad249999616ad867ba069f9
Ajay Tak <ajytak@gmail.com>
2022-07-30 16:22:27 +0530

    add new dynamics
    Dynamics added corresponds to that of a rigid satellite

--- Controller/PID/co_propagating_w.py
+++ Controller/PID/co_propagating_w.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 
-def co_dynamic_function(K, T_i, W_i2, W_i3, w_i2, w_i3, I_i2, I_i3, J_par):
+def co_dynamic_function(I_i1, I_i2, I_i3, T_i1, w_i2, w_i3):
     
     """
     > K is defined in the function below
@@ -14,10 +14,12 @@ def co_dynamic_function(K, T_i, W_i2, W_i3, w_i2, w_i3, I_i2, I_i3, J_par):
     > J_par is the moment of inertia of the reaction wheel about the spin axis
     """
 
-    return ((T_i)+(w_i2*w_i3*(I_i3-I_i2))+(J_par*((W_i3*w_i2)-(W_i2*w_i3))))/(K)
+    w_i1_dot = ((w_i2*w_i3*(I_i2-I_i3)) + T_i1)/I_i1
+
+    return w_i1_dot
 
 
-def co_propagating_w_i(w_i1, w_i2, w_i3, I_i1, I_i2, I_i3, T_i1, T_i2, T_i3, W_i1, W_i2, W_i3, J_perp, J_par, h):
+def co_propagating_w_i(w_1, w_2, w_3, I_1, I_2, I_3, T_1, T_2, T_3, h):
 
     """
     > w_i1, w_i2, w_i3 are components of cubesat's angular velocity wrt to earth's inertial frame expressed in body frame
@@ -28,28 +30,25 @@ def co_propagating_w_i(w_i1, w_i2, w_i3, I_i1, I_i2, I_i3, T_i1, T_i2, T_i3, W_i
     > J_perp is the moment of inertia of the reaction wheel about an axis perpendicular to its spin axis
     > h is time step
     """
-
-    Ki1 = I_i1 + (2*J_perp)
-    Ki2 = I_i2 + (2*J_perp) 
-    Ki3 = I_i3 + (2*J_perp)  
+  
     """
     use of the above constant will be clear in the further steps
     """
 
-    ai1 = co_dynamic_function(Ki1, T_i1, W_i2, W_i3, w_i2, w_i3, I_i2, I_i3, J_par)
-    ai2 = co_dynamic_function(Ki2, T_i2, W_i3, W_i1, w_i3, w_i1, I_i3, I_i1, J_par)
-    ai3 = co_dynamic_function(Ki3, T_i3, W_i1, W_i2, w_i1, w_i2, I_i1, I_i2, J_par)
-    bi1 = co_dynamic_function(Ki1, T_i1, W_i2, W_i3, w_i2 + (0.5*h*ai2), w_i3 + (0.5*h*ai3), I_i2, I_i3, J_par)
-    bi2 = co_dynamic_function(Ki2, T_i2, W_i3, W_i1, w_i3 + (0.5*h*ai3), w_i1 + (0.5*h*ai1), I_i3, I_i1, J_par)
-    bi3 = co_dynamic_function(Ki3, T_i3, W_i1, W_i2, w_i1 + (0.5*h*ai1), w_i2 + (0.5*h*ai2), I_i1, I_i2, J_par)
-    ci1 = co_dynamic_function(Ki1, T_i1, W_i2, W_i3, w_i2 + (0.5*h*bi2), w_i3 + (0.5*h*bi3), I_i2, I_i3, J_par)
-    ci2 = co_dynamic_function(Ki2, T_i2, W_i3, W_i1, w_i3 + (0.5*h*bi3), w_i1 + (0.5*h*bi1), I_i3, I_i1, J_par)
-    ci3 = co_dynamic_function(Ki3, T_i3, W_i1, W_i2, w_i1 + (0.5*h*bi1), w_i2 + (0.5*h*bi2), I_i1, I_i2, J_par)
-    di1 = co_dynamic_function(Ki1, T_i1, W_i2, W_i3, w_i2 + (h*ci2), w_i3 + (h*ci3), I_i2, I_i3, J_par)
-    di2 = co_dynamic_function(Ki2, T_i2, W_i3, W_i2, w_i3 + (h*ci3), w_i1 + (h*ci1), I_i3, I_i1, J_par)
-    di3 = co_dynamic_function(Ki3, T_i3, W_i1, W_i3, w_i1 + (h*ci1), w_i2 + (h*ci2), I_i1, I_i2, J_par)
-    w_i1_next = w_i1 + ((h/6)*(ai1 + (2*bi1) + (2*ci1) + (di1)))
-    w_i2_next = w_i2 + ((h/6)*(ai2 + (2*bi2) + (2*ci2) + (di2)))
-    w_i3_next = w_i3 + ((h/6)*(ai3 + (2*bi3) + (2*ci3) + (di3)))
+    a1 = co_dynamic_function(I_1, I_2, I_3, T_1, w_2, w_3)
+    a2 = co_dynamic_function(I_2, I_3, I_1, T_2, w_3, w_1)
+    a3 = co_dynamic_function(I_3, I_1, I_2, T_3, w_1, w_2)
+    b1 = co_dynamic_function(I_1, I_2, I_3, T_1, w_2 + (0.5*h*a2), w_3 + (0.5*h*a3))
+    b2 = co_dynamic_function(I_2, I_3, I_1, T_2, w_3 + (0.5*h*a3), w_1 + (0.5*h*a1))
+    b3 = co_dynamic_function(I_3, I_1, I_2, T_3, w_1 + (0.5*h*a1), w_2 + (0.5*h*a2))
+    c1 = co_dynamic_function(I_1, I_2, I_3, T_1, w_2 + (0.5*h*b2), w_3 + (0.5*h*b3))
+    c2 = co_dynamic_function(I_2, I_3, I_1, T_2, w_3 + (0.5*h*b3), w_1 + (0.5*h*b1))
+    c3 = co_dynamic_function(I_3, I_1, I_2, T_3, w_1 + (0.5*h*b1), w_2 + (0.5*h*b2))
+    d1 = co_dynamic_function(I_1, I_2, I_3, T_1, w_2 + (h*c2), w_3 + (h*c3))
+    d2 = co_dynamic_function(I_2, I_3, I_1, T_2, w_3 + (h*c3), w_1 + (h*c1))
+    d3 = co_dynamic_function(I_3, I_1, I_2, T_3, w_1 + (h*c1), w_2 + (h*c2))
+    w_1_next = w_1 + ((h/6)*(a1 + (2*b1) + (2*c1) + (d1)))
+    w_2_next = w_2 + ((h/6)*(a2 + (2*b2) + (2*c2) + (d2)))
+    w_3_next = w_3 + ((h/6)*(a3 + (2*b3) + (2*c3) + (d3)))
 
-    return np.array([w_i1_next, w_i2_next, w_i3_next])
+    return np.array([w_1_next, w_2_next, w_3_next])
